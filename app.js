0ba79d4852574ef11b2d72f2518a085038d06251
roqtund <roqtund@gmail.com>
2023-05-08 19:50:33 +0100

    Added swagger documentation

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const express = require("express");
 const app = express();
 
@@ -19,8 +25,10 @@ const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 
 app.get("/", (req, res) => {
-  res.send(process.env.message || "Env not working.");
+  res.send("<h1>Company Directory API</h1><a href='/api-docs'>Documentation</a>");
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.set('trust proxy', 1);
 app.use(rateLimit({
